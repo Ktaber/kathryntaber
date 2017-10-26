b53b9c7ca9b520599ea8c717255bfabafde75e13
ktaber <Taber.kathryn@gmail.com>
2017-10-26 16:20:29 -0400

    hmwk4 image fixes

--- Week_04_Intro_Programming/JavaScript Relaxr/js/index.js
+++ Week_04_Intro_Programming/JavaScript Relaxr/js/index.js
@@ -1,40 +1,58 @@
+var linkElements= document.getElementsByTagName('a');
+
+
+
+// stop links from jumping on page
+for (var i=0; i<linkElements.length; i++) {
+	linkElements[i].onclick = function(event) {
+		console.log(event);
+	event.preventDefault();
+	}
+}
+
+
+
+// Toggle between readmore and readless
 var readmoreElement = document.getElementsByClassName('readmore');
 var readlessElement = document.getElementsByClassName('readless');
-var learnmoretextElement = document.getElementById('learnmoretext');
-var learnmoreElement = document.getElementsByClassName('learnmore');
 var showthisElement = document.getElementById('show-this-on-click');
 
 
 
+console.dir(readmoreElement); //Array
+console.dir(readlessElement); //Array
+console.dir(showthisElement); //Object
+
+
+readmoreElement[0].onclick = function () {
+	readmoreElement[0].style.display = "none";
+	readlessElement[0].style.display = "block";
+	showthisElement.style.display = "block";
+};
 
 
 
+readlessElement[0].onclick = function () {
+	readlessElement[0].style.display = "none";
+	readmoreElement[0].style.display = "block";
+	showthisElement.style.display = "none";
+};
 
 
-// console.log(readmoreElement);
 
+// show learn more text
 
+var learnmoretextElement = document.getElementById('learnmoretext');
+var learnmoreElement = document.getElementsByClassName('learnmore');
 
-console.dir(readmoreElement);
-readmoreElement.onclick = function () {
-	readmore.display = "hide";
-	readless.display = "show";
-	readless.Text.display = "show";
-	show-this-on-click.display = "show";
-}
-
-console.dir(readlessElement);
-readlessElement.onclick = function () {
-	// do the opposite
-	readless.display = "hide";
-	readmore.display = "show";
-	readmore.Text.display = "show";
-	show-this-on-click.display = "hide";
-}
 
 console.dir(learnmoreElement);
-learnmoreElement.onclick = function () {
-	learnmoretext.display = "show";
-	learnmore.display = "hide";
 
+learnmoreElement[0].onclick = function(event) {
+	learnmoretextElement.style.display = "block";
+	learnmoreElement[0].style.display = "none";
+	event.preventDefault(); 
 }
+
+
+
