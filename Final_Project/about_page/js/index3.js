9f9d682eca2c6b71239a741a8d4a95936256225f
ktaber <Taber.kathryn@gmail.com>
2017-11-21 17:00:44 -0500

    final project about page

---
+++ Final_Project/about_page/js/index3.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+
+
+	$('h3').click(function(){
+    	$('ul').show();
+    	$('.background').show();
+
+	});
+
+	$('.matters').click(function(){
+		$('#why').show();
+	});
+});
