25e2d9b14ed173830ceb8ad6a3f6e199a4770bbe
ktaber <Taber.kathryn@gmail.com>
2017-11-21 17:02:39 -0500

    final project recipes page

---
+++ Final_Project/recipes/js/index4.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+
+
+
+	$('.am').click(function(){
+    	$('.tofu1').show();
+    	$('.stuff').show();
+    	$('.one').show();
+    	$(img)
+    });
+
+    $('.lunch').click(function(){
+    	$('.tuna').show();
+    	$('.stuff2').show();
+    	$('.two').show();
+    });
+
+    $('.dinner').click(function(){
+    	$('.rice').show();
+    	$('.stuff3').show();
+    	$('.three').show();
+    });
+
+    $('.sweets').click(function(){
+    	$('.cupcakes').show();
+    	$('.stuff4').show();
+    	$('.four').show();
+    });
+
+    $('.snacks').click(function(){
+    	$('.chick').show();
+    	$('.stuff5').show();
+    	$('.five').show();
+    	console.log('hey');
+	});
+
+ 	event.preventDefault();
+});
